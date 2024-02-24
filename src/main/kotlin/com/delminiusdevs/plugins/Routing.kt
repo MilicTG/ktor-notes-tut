888b9537a2baa7d2c916fed866310542b4d025c1
Ivan Milic <milic.tg@gmail.com>
2024-02-24 11:26:07 +0100

    Update Routing.kt

--- src/main/kotlin/com/delminiusdevs/plugins/Routing.kt
+++ src/main/kotlin/com/delminiusdevs/plugins/Routing.kt
@@ -10,7 +10,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Mafija je svuda oko nas")
+            call.respondText("Bošnjak mafija je svuda oko tebe!!!")
         }
 
         userRoutes()
